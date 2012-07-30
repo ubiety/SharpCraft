ce1c98b0e6686593018ec54eddf036ff1701eb90
Dieter Lunn <coder2000@gmail.com>
2012-07-29 22:14:33 -0500

    Added settings powered by YAML.

---
+++ SharpCraft/Settings.cs
@@ -0,0 +1,23 @@
+﻿namespace SharpCraft
+{
+	/// <summary>
+	/// Stores the server settings.
+	/// </summary>
+	public class Settings
+	{
+		/// <summary>
+		/// Name of the world file to load.
+		/// </summary>
+		public string WorldName { get; set; }
+
+		/// <summary>
+		/// Seed used to generate a new world.
+		/// </summary>
+		public string Seed { get; set; }
+
+		/// <summary>
+		/// Port to connect to.
+		/// </summary>
+		public int Port { get; set; }
+	}
+}
--- SharpCraft/SharpCraft.cs
+++ SharpCraft/SharpCraft.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using SharpCraft.Net;
 using SharpCraft.World;
+using YamlDotNet.RepresentationModel.Serialization;
 
 namespace SharpCraft
 {
@@ -16,7 +19,24 @@ namespace SharpCraft
 		/// </summary>
 		public const int ProtocolVersion = 29;
 
+		private static readonly string SettingsFile =
+			Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SharpCraft", "settings.yml");
+
+		private readonly Settings _settings;
 		private TcpListener _listener;
+
+		/// <summary>
+		/// Creates a new instance and initializes the settings.
+		/// </summary>
+		/// <param name="fileName">Full path to settings file.</param>
+		public SharpCraft(string fileName = null)
+		{
+			YamlSerializer yaml = YamlSerializer.Create(typeof (Settings));
+			using (var settings = new StreamReader(fileName ?? SettingsFile))
+			{
+				_settings = yaml.Deserialize(settings) as Settings;
+			}
+		}
 
 		/// <summary>
 		/// The map that is hosted by this server.
@@ -28,14 +48,13 @@ namespace SharpCraft
 		/// </summary>
 		public List<Client> Clients { get; private set; }
 
-
 		/// <summary>
 		/// Starts the server instance. Optionally specify the local end point to be used for incoming connections.
 		/// </summary>
 		/// <param name="local">End point to bind on. If not specified default will be used.</param>
 		public void Start(IPEndPoint local = null)
 		{
-			IPEndPoint ep = local ?? new IPEndPoint(IPAddress.Parse("0.0.0.0"), 25575);
+			IPEndPoint ep = local ?? new IPEndPoint(IPAddress.Parse("0.0.0.0"), _settings.Port);
 
 			_listener = new TcpListener(ep);
 			_listener.Start();
